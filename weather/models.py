c0d68441c6e529f02ba7f659b30f17cc05dfb9fe
Gichie <gichie@mail.ru>
2025-04-01 16:28:52 +0300

    migrate(delete user model)

--- weather/models.py
+++ weather/models.py
@@ -2,12 +2,11 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 
-
 class Location(models.Model):
     name = models.CharField(max_length=190)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='locations')
-    latitude = models.DecimalField(max_digits=8, decimal_places=5)
-    longitude = models.DecimalField(max_digits=8, decimal_places=5)
+    latitude = models.DecimalField(max_digits=7, decimal_places=4)
+    longitude = models.DecimalField(max_digits=7, decimal_places=4)
 
     def __str__(self):
         return self.name
@@ -16,5 +15,5 @@ class Location(models.Model):
         verbose_name = 'Локация'
         verbose_name_plural = 'Локации'
         constraints = [
-            models.UniqueConstraint(fields=['user', 'latitude', 'longitude'], name='unique_user_location_coordinates')
+            models.UniqueConstraint(fields=['user', 'latitude', 'longitude'], name='unique_user_coordinates')
         ]
